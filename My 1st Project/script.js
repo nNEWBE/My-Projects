ab2baa9d034803eb4a9b122b3f29d6c40278d07e
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2024-01-13 01:16:52 +0600

    third commit

--- My 1st Project/script.js
+++ My 1st Project/script.js
@@ -1,6 +1,13 @@
 // toggle icon navabr
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-//scroll sectiions
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sectiions
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -11,16 +18,29 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
         if (top >= offset && top < offset + height) {
-            //active navbar links
+
+            // active navbar links
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats an scroll use this
+        else {
+            sec.classList.remove('show-animate');
         }
     });
     // sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-    let header = document.querySelector("header");
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
-    header.classList.toggle("sticky", window.scrollY > 100);
-};
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
